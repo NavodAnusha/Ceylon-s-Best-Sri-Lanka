31b821eaf0251ed0fa9460d179702f756e66c6db
navodanushan@outlook.com <Navod123>
2019-09-18 11:50:37 +0530

    updated

--- BlogHome.php
+++ BlogHome.php
@@ -1,39 +1,39 @@
-<body>
 <?php
-if (isset ($_POST ['submit']))
-{
-	include('Connection.php');
-	
-	$errorMessage = "Connection Failed";
-	
-	$Name=$_POST['Name'];
-	$Email=$_POST['Email'];
-	$Title=$_POST['Title'];
-	$Comment=$_POST['Comment'];
 
-	
-	if(!empty($errorMessage))
-	{
-		echo("<p>There is an error with your form:</p>\n");
-		echo("<ul>" . $errorMessage . "</ul>\n");
-	}
-	
-	else
-	{
-		$sql = "INSERT INTO blog VALUES('$name','$email','$title','$comment')";
-	
-		$data=mysqli_query($conn,$sql);
-		
- if(!$data)
-{
-	die('could not connect:'. mysql_error());
-}
-	echo 'Enter data successfully';
+//Block 1
+$user = "user"; //Enter the user name
+$password = "password"; //Enter the password
+$host = "host"; //Enter the host
+$dbase = "database"; //Enter the database
+$table = "table"; //Enter the table name
 
-	}
-}
-else
+//Block 2
+$firstname= $_POST['firstname_entered'];
+$lastname= $_POST['lastname_entered'];
+$email= $_POST['email_entered'];
+
+//Block 3
+$connection= mysql_connect ($host, $user, $password);
+if (!$connection)
 {
-echo "Your form is not submitted yet, please fill the form and visit again" ;
+die ('Could not connect:' . mysql_error());
 }
+mysql_select_db($database, $connection);
+
+
+//Block 4
+$username_table= mysql_query( "SELECT username FROM $table WHERE username= '$username'" ) 
+or die("SELECT Error: ".mysql_error()); 
+
+
+//Block 5
+mysql_query("INSERT INTO $table (column1, column2, column3)
+VALUES (value1, value2, value 3)");
+
+//Block 6
+echo 'You have been added.';
+
+//Block 7
+mysql_close($connection);
+
 ?>
